9e2627318aae2e5e27692f550b55035f9a21bed7
Usman <mizmaar3@hotmail.com>
2016-08-28 00:09:55 +0200

    Dummy data as separate file

--- src/js/items-store.jsx
+++ src/js/items-store.jsx
@@ -2,25 +2,8 @@ let Reducer = require('./reducer.js');
 let Actions = require('./actions.js');
 let Redux = require('redux');
 let ReactRedux = require('react-redux');
+let Data = require('../data/data.js');
 
-let items = [
-  {
-    id: 1,
-    title: "Item 1",
-    balance: 200,
-  },
-  {
-    id: 2,
-    title: "item 2",
-    balance: 400,
-  },
-  {
-    id: 3,
-    title: "item 3",
-    balance: 1000,
-  }
-];
-
-let ItemsStore = Redux.createStore(Reducer, [...items]);
+let ItemsStore = Redux.createStore(Reducer, [...Data]);
 
 module.exports = ItemsStore;
